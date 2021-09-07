61e54e6738e6af4bf7ee9167ebd5a335183a0917
lmay931 <lawrence.may@outlook.com>
2021-09-07 16:14:55 +1200

    added recyclerview

--- app/src/main/java/com/example/ishop/ItemAdapter.kt
+++ app/src/main/java/com/example/ishop/ItemAdapter.kt
@@ -1,12 +1,13 @@
 package com.example.ishop
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.ishop.database.GroceryItem
 
-class ItemAdapter : RecyclerView.Adapter<TextItemViewHolder>() {
+class ItemAdapter : RecyclerView.Adapter<ItemAdapter.ViewHolder>() {
     var data = listOf<GroceryItem>()
     set(value) {
         field = value
@@ -15,15 +16,17 @@ class ItemAdapter : RecyclerView.Adapter<TextItemViewHolder>() {
 
     override fun getItemCount() = data.size
 
-    override fun onBindViewHolder(holder: TextItemViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = data[position]
-        holder.textView.text = item.Item.toString()
+        holder.item.text = item.Item
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TextItemViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val layoutInflater = LayoutInflater.from(parent.context)
-        val view = layoutInflater.inflate(R.layout.text_item_view,parent,false) as TextView
-        return TextItemViewHolder(view)
+        val view = layoutInflater.inflate(R.layout.list_item_grocery_item,parent,false)
+        return ViewHolder(view)
     }
-
+    class ViewHolder(private val itemView: View): RecyclerView.ViewHolder(itemView) {
+        val item : TextView = itemView.findViewById(R.id.item_name)
+    }
 }
