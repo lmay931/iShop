b544bbb866d2b07e7eca7e2e944b84ba0fd4966d
lmay931 <lawrence.may@outlook.com>
2021-09-08 20:57:59 +1200

    added recyclerview

---
+++ app/src/main/java/com/example/ishop/ViewModelFactories.kt
@@ -0,0 +1,81 @@
+package com.example.ishop
+
+import android.app.Application
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.ishop.database.GroceryItemListDatabaseDao
+
+class ChooseNewNameViewModelFactory (
+    private val dataSource: GroceryItemListDatabaseDao,
+    private val application: Application) : ViewModelProvider.Factory {
+    @Suppress("unchecked_cast")
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(ChooseNewNameViewModel::class.java)) {
+            return ChooseNewNameViewModel(dataSource, application) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
+
+class ManageCategoriesViewModelFactory (
+    private val dataSource: GroceryItemListDatabaseDao,
+    private val listName: String,
+    private val application: Application) : ViewModelProvider.Factory {
+    @Suppress("unchecked_cast")
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(ManageCategoriesViewModel::class.java)) {
+            return ManageCategoriesViewModel(dataSource, listName, application) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
+
+class ManageListsViewModelFactory (
+    private val dataSource: GroceryItemListDatabaseDao,
+    private val application: Application) : ViewModelProvider.Factory {
+    @Suppress("unchecked_cast")
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(ManageListsViewModel::class.java)) {
+            return ManageListsViewModel(dataSource, application) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
+
+class NewListViewModelFactory (
+    private val dataSource: GroceryItemListDatabaseDao,
+    private val listName: String,
+    private val application: Application) : ViewModelProvider.Factory {
+    @Suppress("unchecked_cast")
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(NewListViewModel::class.java)) {
+            return NewListViewModel(dataSource, listName, application) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
+
+class RetrieveExistingListViewModelFactory (
+    private val dataSource: GroceryItemListDatabaseDao,
+    private val application: Application) : ViewModelProvider.Factory {
+    @Suppress("unchecked_cast")
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(RetrieveExistingListViewModel::class.java)) {
+            return RetrieveExistingListViewModel(dataSource, application) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
+
+class ShoppingViewModelFactory (
+    private val dataSource: GroceryItemListDatabaseDao,
+    private val listName: String,
+    private val application: Application) : ViewModelProvider.Factory {
+    @Suppress("unchecked_cast")
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(ShoppingViewModel::class.java)) {
+            return ShoppingViewModel(dataSource, listName, application) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
