5f835e1dfd97fdb489829bb0ba938a13c59535d9
lmay931 <lawrence.may@outlook.com>
2021-09-08 16:23:06 +1200

    added recyclerview

--- app/src/main/java/com/example/ishop/ManageCategoriesFragment.kt
+++ app/src/main/java/com/example/ishop/ManageCategoriesFragment.kt
@@ -1,12 +1,10 @@
-package com.example.ishop.manageCategories
-
+package com.example.ishop
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import com.example.ishop.*
 import com.example.ishop.database.GroceryItemListDatabase
 import com.example.ishop.databinding.FragmentManageCurrentListsBinding
 
@@ -15,7 +13,8 @@ class ManageCategoriesFragment : Fragment() {
                               savedInstanceState: Bundle?): View {
 
         val binding: FragmentManageCurrentListsBinding = DataBindingUtil.inflate(
-            inflater, R.layout.fragment_manage_categories, container, false)
+            inflater, R.layout.fragment_manage_categories, container, false
+        )
 
         val application = requireNotNull(this.activity).application
         val dataSource = GroceryItemListDatabase.getInstance(application).groceryItemDatabaseDao
--- app/src/main/java/com/example/ishop/ManageListsFragment.kt
+++ app/src/main/java/com/example/ishop/ManageListsFragment.kt
@@ -1,4 +1,4 @@
-package com.example.ishop.manageLists
+package com.example.ishop
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -6,7 +6,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import com.example.ishop.*
 import com.example.ishop.database.GroceryItemListDatabase
 import com.example.ishop.databinding.FragmentManageCurrentListsBinding
 
@@ -15,7 +14,8 @@ class ManageListsFragment : Fragment() {
                               savedInstanceState: Bundle?): View {
 
         val binding: FragmentManageCurrentListsBinding = DataBindingUtil.inflate(
-            inflater, R.layout.fragment_manage_current_lists, container, false)
+            inflater, R.layout.fragment_manage_current_lists, container, false
+        )
 
         val application = requireNotNull(this.activity).application
         val dataSource = GroceryItemListDatabase.getInstance(application).groceryItemDatabaseDao
